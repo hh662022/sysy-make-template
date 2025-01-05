25bb5b93397eb637b819fd7c3e0576c9d1632094
hh662022 <104916036+hh662022@users.noreply.github.com>
2025-01-05 10:10:52 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,52 @@
+
 #include <cassert>
-#include <fstream>
-#include <unordered_map>
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <sstream>
+#include "koopa.h"
+#include "AST.hpp"
+#include "riskv.hpp"
 
 using namespace std;
 
-std::unordered_map<char, const char *> generator = {
-    {'k', R"(fun @main(): i32 {
-%entry:
-  ret 0
-}
-)"},
-    {'r', R"(  .text
-  .globl main
-main:
-  li a0, 0
-  ret
-)"},
-    {'p', R"(  .text
-  .globl main
-main:
-  li a0, 0
-  ret
-)"},
-};
+extern FILE *yyin;
+extern int yyparse(unique_ptr<BaseAST> &ast);
 
 int main(int argc, const char *argv[]) {
-  assert(argc == 5);
-  ofstream ofs(argv[4]);
-  ofs << generator[argv[1][1]];
-  return 0;
+    assert(argc == 5);
+    string mode = (string)argv[1];
+    auto input = argv[2];
+    auto output = argv[4];
+    yyin = fopen(input, "r");
+    assert(yyin);
+    freopen(output, "w", stdout);
+    unique_ptr<BaseAST> ast;
+    auto ret = yyparse(ast);
+    assert(!ret);
+    if(mode == "-koopa"){
+        ast->Dump(); 
+    }
+    else{// RISC-V
+        stringstream ss;
+        streambuf *buf = cout.rdbuf();
+        cout.rdbuf (ss.rdbuf());
+    ast->Dump();
+        string str = ss.str();
+        const char * IRStr = str.data();
+
+        cout.rdbuf(buf);
+        koopa_program_t program;
+        koopa_error_code_t ret = koopa_parse_from_string(IRStr, &program);
+        assert(ret == KOOPA_EC_SUCCESS); 
+        koopa_raw_program_builder_t builder = koopa_new_raw_program_builder();
+        koopa_raw_program_t raw = koopa_build_raw_program(builder, program);
+        Visit(raw);
+
+        koopa_delete_program(program);
+        koopa_delete_raw_program_builder(builder);
+    }
+
+    return 0;
 }
